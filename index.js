6ef457f2845eb3b3c469b54cc1b933e372c236ca
jili <jili@heidrick.com>
2017-07-19 15:33:43 -0500

    Add postgres sql

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var app = express();
 
@@ -13,8 +15,47 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
 });
 
 
+const { Pool, Client } = require('pg');
+
+
+app.get('/db', function (request, response) {
+
+  const pool = new Pool({
+    user: 'kkcqruwcwogqgt',
+    host: 'ec2-23-21-96-159.compute-1.amazonaws.com',
+    database: 'd61ulk3fo1decn',
+    password: 'ecac9473c819af22779e9568e5e444a1847f31e9b2d50ace2a302f51a57867c8',
+    port: 5432,
+    ssl:true
+  })
+
+  pool.query('SELECT * FROM test_table', (err, res) => {
+
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: res.rows} ); }
+
+
+    pool.end();
+  })
+
+
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
